67014e875b42b7b25cef63c8843c4b8836242f10
Mats01 <matej.butkovic@gmail.com>
2022-12-03 22:56:13 +0100

    rename some vars

--- 03/solution.ts
+++ 03/solution.ts
@@ -17,7 +17,7 @@ const solution2 = readFileSync('./input.txt', 'utf-8')
   .map((line) => line.split('').map((char) => char.charCodeAt(0)))
   .map((line) => line.map((char) => char >= 97 ? char - 96 : char - 64 + 26))
   .reduce((acc, _, index, arr) => (index + 3 > arr.length) || index % 3 !== 0 ? acc : [...acc, [arr[index], arr[index + 1], arr[index + 2]]], [] as number[][][])
-  .map(([firstHalf, secondHalf, thirdHalf]) => firstHalf.filter(num => secondHalf.includes(num) && thirdHalf.includes(num)))
+  .map(([firstElf, secondElf, thirdElf]) => firstElf.filter(num => secondElf.includes(num) && thirdElf.includes(num)))
   .map((line) => line.filter((num, index) => line.indexOf(num) === index))
   .flatMap((line) => line)
   .reduce((acc, curr) => acc + curr, 0);
