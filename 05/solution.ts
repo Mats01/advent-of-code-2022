15976567975c581433c0c2a248ccb34cb6677af5
Mats01 <matej.butkovic@gmail.com>
2022-12-05 11:26:59 +0100

    restacking in one line

--- 05/solution.ts
+++ 05/solution.ts
@@ -2,40 +2,40 @@ import { readFileSync } from 'fs';
 
 
 
-const solution1 = readFileSync('./input.txt', 'utf-8')
-  .split('\n\n')
-  .map((line) => line.split('\n'))
-  .map((line, index) => index !== 0 ? line : line.reduce((acc, curr) => {
+const initialStack = readFileSync('./input.txt', 'utf-8')
+  .split('\n\n')[0]
+  .split('\n')
+  .reduce((acc, curr) => {
     for (let i = 0; i < curr.length - 1; i++) {
       if ((i - 1) % 4 === 0 && curr[i] !== ' ' && curr[i].charCodeAt(0) > 57) {
-        acc[(i - 1) / 4] = [...(acc[(i - 1) / 4] || []), curr[i]];
+        acc[(i - 1) / 4 + 1] = [...(acc[(i - 1) / 4 + 1] || []), curr[i]];
       }
     }
     return acc;
-  }, [] as any[]))
-  .reduce((acc, curr, index) => {
-    if (index === 0) {
-      return curr
-    };
-    const newAcc = acc as string[][];
-    for (const input of curr) {
-      const ammount = parseInt(input.split(' ')[1]);
-      const sourceIndex = parseInt(input.split(' ')[3]) - 1;
-      const destinationIndex = parseInt(input.split(' ')[5]) - 1;
+  }, {} as { [key: number]: string[] });
 
 
-      for (let i = 0; i < ammount; i++) {
-        const source = newAcc[sourceIndex];
-        if (!source) break;
-        const first = source.shift();
-        first && newAcc[destinationIndex].unshift(first);
-      }
+const moveFro = (arr: string[], elems: number) => {
+  // get first elems elements of arr
+  return;
 
-    }
-    return newAcc;
-  }, [] as any[]);
+}
 
-const result = solution1.reduce((acc, curr) => acc + (curr[0] || ''), '');
+
+const solution1 = readFileSync('./input.txt', 'utf-8')
+  .split('\n\n')[1]
+  .split('\n')
+  .reduce((acc, curr) => ({
+    ...acc,
+    [parseInt(curr.split(' ')[5])]: [
+      ...acc[parseInt(curr.split(' ')[3])].slice(0, parseInt(curr.split(' ')[1])).reverse(),
+      ...acc[parseInt(curr.split(' ')[5])]
+    ],
+    [parseInt(curr.split(' ')[3])]: acc[parseInt(curr.split(' ')[3])].slice(parseInt(curr.split(' ')[1])),
+  }
+  ), initialStack);
+
+const result = Object.values(solution1).reduce((acc, curr) => acc + (curr[0] || ''), '');
 
 
 const solution2 = readFileSync('./input.txt', 'utf-8')
@@ -46,5 +46,11 @@ const solution2 = readFileSync('./input.txt', 'utf-8')
 
 console.log(result);
 console.log(solution1);
-console.log(solution2);
+// console.log(solution2);
 
+
+
+
+// .map((value, index, arr) => index === 0 ? value : [arr[0], value]).slice(1)
+
+console.log(initialStack);
