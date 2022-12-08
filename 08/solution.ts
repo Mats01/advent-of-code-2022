efaaf3e41aab4cdaef6822be0a06c8b5377da69a
Mats01 <matej.butkovic@gmail.com>
2022-12-08 10:23:53 +0100

    slightly refactor day 8 part 1

--- 08/solution.ts
+++ 08/solution.ts
@@ -1,46 +1,36 @@
 import { readFileSync } from 'fs';
 
-const input = readFileSync('input.txt', 'utf8').split('\n').map(line => line.split('').map(tree => parseInt(tree)));
+const input = readFileSync('input.txt', 'utf8').split('\n').map(line => line.split('').map(tree => parseInt(tree))) as number[][];
 
 const width = input[0].length;
 const height = input.length;
 
 const outerVisible = 2 * width + 2 * height - 4;
 
-let innerVisible: string[][] = [];
-for (let i = 0; i < height; i++) {
-  innerVisible[i] = [];
-  for (let j = 0; j < width; j++) {
-    innerVisible[i][j] = '..';
 
+const innerVisible = Array.from({ length: input.length }, () => Array.from({ length: input[0].length }, () => '..'));
+
+
+input.forEach((row, i) => {
+  if (i === 0 || i === height - 1) {
+    return;
   }
-}
-
-
-
-
-
-
-for (let i = 1; i < height - 1; i++) {
-  // visible from the left
-  let last = input[i][0];
-  for (let j = 1; j < width - 1; j++) {
-    if (input[i][j] > last) {
-      last = input[i][j];
+  row.reduce((acc, curr, j) => {
+    if (curr > acc) {
       innerVisible[i][j] = "🌲";
+      return curr;
     }
-  }
-
-  // visible from the right
-  last = input[i][width - 1];
-  for (let j = width - 2; j >= 1; j--) {
-    if (input[i][j] > last) {
-      last = input[i][j];
+    return acc;
+  }, row[0] as number);
+  row.reduceRight((acc, curr, j) => {
+    if (curr > acc) {
       innerVisible[i][j] = "🌲";
+      return curr;
     }
-  }
-}
+    return acc;
+  }, row[width - 1] as number);
 
+});
 for (let i = 1; i < width - 1; i++) {
   // visible from the top
   let last = input[0][i];
@@ -61,6 +51,7 @@ for (let i = 1; i < width - 1; i++) {
   }
 }
 
+
 const innerVisibleSum = innerVisible.reduce((acc, row) => acc + row.reduce((acc, col) => acc + (col !== '..' ? 1 : 0), 0), 0);
 
 
@@ -74,5 +65,5 @@ for (let i = 0; i < height; i++) {
 // console.log('outerVisible', outerVisible);
 // console.log('innerVisible', innerVisibleSum);
 
-
-console.log(outerVisible + innerVisibleSum);
+const solution1 = outerVisible + innerVisibleSum;
+console.log("solution1:", solution1);
