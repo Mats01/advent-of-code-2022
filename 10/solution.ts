1c5e7f7352fa2acdb50505f4834019a36b04dd3c
Mats01 <matej.butkovic@gmail.com>
2022-12-10 16:30:22 +0100

    day 10 part1 onleliner

--- 10/solution.ts
+++ 10/solution.ts
@@ -5,18 +5,10 @@ const input = readFileSync('input.txt', 'utf8').split('\n').map(line => [line.sp
 
 
 
-const solution1 = input.
-  reduce((acc, line) => {
-
-    const command = line[0];
-    const amount = line[1];
-
-    if (command === 'noop') {
-      return [...acc, acc.at(-1)];
-    }
-
-    return [...acc, acc.at(-1), acc.at(-1) + amount];
-  }, [1]);
+const solution1 = (readFileSync('input.txt', 'utf8')
+  .split('\n')
+  .map(line => [line.split(' ')[0], parseInt(line.split(' ')[1])]) as any[])
+  .reduce((acc, line) => (line[0] === 'noop') ? [...acc, acc.at(-1)] : [...acc, acc.at(-1), acc.at(-1) + line[1]], [1]);
 
 
 const solution2 = input.length;
